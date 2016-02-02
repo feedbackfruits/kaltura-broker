699ec8b149765300cf88d6f6d9c08d56db38470f
Joost Verdoorn <jpverdoorn@gmail.com>
2016-02-02 16:30:08 +0100

    Add kaltura session broker

---
+++ index.js
@@ -0,0 +1,32 @@
+// Load .env file
+require('dotenv').config({silent: true});
+
+const request    = require('request'),
+      express    = require('express');
+
+const JSON_FORMAT = 1;
+const server = express();
+
+if (typeof process.env.FBF_TOKEN === 'undefined' || process.env.FBF_TOKEN.length < 32) {
+  console.error("Should set environment variable FBF_TOKEN of a length of at least 32 characters");
+  process.exit(1);
+}
+
+server.get('/session', (req, res) => {
+  if (req.headers['x-fbf-token'] !== process.env.FBF_TOKEN) return res.status(403).end();
+
+  request.post({
+    url: 'http://www.kaltura.com/api_v3/',
+    form: {
+      service: 'session',
+      action: 'start',
+      format: JSON_FORMAT,
+      secret: process.env.KALTURA_SECRET,
+      userId: process.env.KALTURA_USER_ID,
+      partnerId: process.env.KALTURA_PARTNER_ID
+    }
+  }).pipe(res);
+});
+
+server.listen(process.env.PORT);
+console.info("Server started, listening on port", process.env.PORT);
